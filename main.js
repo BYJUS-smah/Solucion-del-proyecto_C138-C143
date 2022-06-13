1b005cbb1e931a9def63469d966e0cde4b7b8885
BYJUS-smah <95708613+BYJUS-smah@users.noreply.github.com>
2022-06-13 11:15:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -27,8 +27,6 @@ scoreRightWrist = 0;
 
 game_status = "";
 
-//var doggo_theme = new Audio("https://saantonandre.github.io/doggo_theme.wav");
-
  function preload() {
   ball_touch_paddel = loadSound("ball_touch_paddel.wav");
   missed = loadSound("missed.wav");
